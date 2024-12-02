ad0c11cdf77e97734c3075d5bd7e96e0ecf533e2
angelmrt <amartinr03@gmail.com>
2024-12-02 13:15:56 +0100

    v1.1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-    root: 'src',
+    root: './src',
     build: {
         outDir: '../docs',
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'src/index.html'), // Archivo principal en ./src
+                about: path.resolve(__dirname, 'src/login.html'), // Archivo de login
+                contact: path.resolve(__dirname, 'src/mainpage.html'), // Archivo de mainpage
+            },
+        },
     },
     base: '/All-for-DJ-S/',
 });
